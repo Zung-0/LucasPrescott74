28ff445da43a407705447e649d140cf6a7b8fbdf
Zung-0 <diego.zuniga.seminario@gmail.com>
2015-09-09 20:39:01 -0500

    Modified the description

--- lucas_search_general.py
+++ lucas_search_general.py
@@ -3,7 +3,9 @@ filename: lucas_search_general.py
 Author: Diego Zuniga and Alex Carrasco
 
 Solves and simulates the general equilibirium of the 
-Lucas and Prescott Search model for any economy.
+Lucas and Prescott Search model for any economy by creating 
+a list of islands as instances of the LucasSearch class
+defined in lucas_search.
 
 Following (1974) Equilibrium Search and Unemployment - Journal oF Economic Theory
 by Robert E. Lucas, Jr. and Edward C. Prescott
@@ -41,6 +43,8 @@ class LucasSearchGeneral(object):
 		Contains the workforce size of each island
 	islands_state : array_like(int), optional(default = [0]*50 + [1]*50)
 		Contains the state of demand of each island
+	growth : scalar(float), optional(default = 0)
+		Rate of growth of the economy's workforce
 	stochastic : Boolean, optional(default = False)
 		Whether the arrival process is stochastic or not
 	Attributes
@@ -51,14 +55,15 @@ class LucasSearchGeneral(object):
 	'''
 
 	def __init__(self, beta=.9, states=[1, 2], transition=[[.9, .1], [.1, .9]], 
-					islands_wf = [50]*100, islands_state = [0]*50 + [1]*50,
+					islands_wf = [50]*100, islands_state = [0]*50 + [1]*50, 
 					stochastic=False): 
 	
 		self.beta = beta
-		self.states = states
-		self.transition = transition
+		self.states = np.asarray(states)
+		self.transition = np.asarray(transition)
 		self.islands_wf = np.asarray(islands_wf)
 		self.islands_state = np.asarray(islands_state)
+		self.population = self.islands_wf.sum()
 		self.stochastic = stochastic
 
 	def change_array(self, l):
@@ -112,7 +117,7 @@ class LucasSearchGeneral(object):
 
 		Returns 
 		-------
-		lam: scalar(float)
+		lambda_star: scalar(float)
 			Value of lambda that makes the workforce change
 			of the complete economy 0. (Fixed workforce)
 		"""
@@ -123,7 +128,7 @@ class LucasSearchGeneral(object):
 		#	Nevertheless, I am not sure I should lose too much sleep
 		#	on that.
 
-		change = lambda l: self.net_moves(l)
+		change = lambda l: self.net_moves(l) 
 		lambda_star = brenth(change, 0, 10)
 		return lambda_star
 	
@@ -142,7 +147,7 @@ class LucasSearchGeneral(object):
 		self.islands_wf = self.islands_wf + change_wf 
 		change = np.random.choice((0,1), p=(.9, .1), size=len(self.islands_wf)) # 1 if the state changes, 0 otherwise
 		self.islands_state = (self.islands_state + change)%2 # Computes the new state
-
+ 
 
 	def simulate(self, T=100, compute_unemployment=False):
 		"""
